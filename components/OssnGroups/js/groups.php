2e898a48177bce8a82396ff34a25c6ee2e69d8bd
Arsalan Shah <manactric@gmail.com>
2014-09-22 22:19:58 +0500

    1.4 release
    Former-commit-id: 42d2b1ab079d14f7fb1058af1058d7d5c0dc958a

--- components/OssnGroups/js/groups.php
+++
@@ -1,7 +0,0 @@
-Ossn.RegisterStartupFunction(function(){
-   $(document).ready(function(){
-	     $('#ossn-group-add').click(function(){
-                      Ossn.MessageBox('groups/add');
-         }); 
-	});
-});
