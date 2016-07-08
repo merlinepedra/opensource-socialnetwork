880453db85f774489784a200886822052ed34a25
lianglee <manactric@gmail.com>
2016-07-09 02:02:06 +0500

    update copyrights
    Former-commit-id: 527bd2acb8d83feca36e220fc3a6205f75d484aa

---
+++ components/OssnAds/locale/ossn.en.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @packageOpen Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014-2016 SOFTLAB24 LIMITED
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+$en = array(
+    'ossnads' => 'Ads Manager',
+    'fields:required' => 'All fields are required!',
+    'ad:created' => 'Ad has been created!',
+    'ad:create:fail' => 'Cannot create ad!',
+    'ad:title' => 'Title',
+    'ad:site:url' => 'Siteurl',
+    'ad:desc' => 'Description',
+    'ad:photo' => 'Photo',
+    'ad:browse' => 'Browse',
+    'ad:clicks' => 'Clicks',
+    'sponsored' => 'SPONSORED',
+	'ad:deleted' => "Ad with the title of '%s' has been successfully deleted.",
+	'ad:delete:fail' => 'Cannot delete ad! Please try again later.',
+	'ad:edited' => 'Ad successfully modified.',
+	'ad:edit:fail' => 'Cannot edit ad! Please try again later.',
+);
+ossn_register_languages('en', $en); 
